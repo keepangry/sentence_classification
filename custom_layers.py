88898ac8c927ea3ad9973e3da68985893c8da117
Yang Sen <0@keepangry.com>
2018-12-11 11:01:35 +0800

    Update custom_layers.py

--- custom_layers.py
+++ custom_layers.py
@@ -18,4 +18,12 @@ def base_embed_lstm_net(max_word_num):
     model.add(layers.Dense(1, activation='sigmoid'))
     return model
 
-
+def base_embed_cnn_lstm_net(max_word_num):
+    model = Sequential()
+    model.add(layers.Embedding(max_word_num, 128))
+    model.add(layers.Dropout(0.25))
+    model.add(layers.Conv1D(64, 5, padding='valid', activation='relu', strides=1))
+    model.add(layers.MaxPooling1D(pool_size=4))
+    model.add(layers.LSTM(64))
+    model.add(layers.Dense(1, activation='sigmoid'))
+    return model
