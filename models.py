cf200d7fed20f2ad852c592a7185b439f7a053d1
Yang Sen <0@keepangry.com>
2018-12-18 16:51:31 +0800

    Update models.py
    add lstm attention model

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
-from custom_layers import base_embed_lstm_net, base_embed_cnn_lstm_net, base_multi_channel_net
+from custom_layers import base_embed_lstm_net, base_embed_cnn_lstm_net, base_multi_channel_net, base_attention_lstm
 import keras
 
 
 def mr_base_model(dataset, vocabulary_size, maxlen, method="base_multi_channel_net"):
     if method == "base_multi_channel_net":
         model = base_multi_channel_net(vocabulary_size, time_steps=maxlen)
+    elif method == "base_attention_lstm":
+        model = base_attention_lstm(vocabulary_size, time_steps=maxlen, type="before")
     elif method == "base_embed_cnn_lstm_net":
         model = base_embed_cnn_lstm_net(vocabulary_size)
     else:
@@ -25,6 +27,6 @@ def mr_base_model(dataset, vocabulary_size, maxlen, method="base_multi_channel_n
                         batch_size=128,
                         validation_data=(dataset.x_val, dataset.y_val),
                         callbacks=callbacks,
-                        verbose=0)
+                        verbose=2)
     return model, history
 
